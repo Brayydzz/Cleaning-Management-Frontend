a1d0261dadfc086eb8e3556e583703f9d8dcbe2f
James Perrin <jamesperrin88@gmail.com>
2021-07-14 16:46:09 +1000

    merge conflicts approved

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,6 +8,9 @@ const Nav = () => {
                 <li><Link to="/">Home</Link></li>
                 <li><Link>Services</Link></li>
                 <li><Link>About</Link></li>
+                {/* DELETE THIS BELOW */}
+                <li><Link to="/dashboard/admin">Dashboard</Link></li>
+                
             </ul>
             <Link to="/bookings"><button>Book Now!</button></Link>
         </nav>
